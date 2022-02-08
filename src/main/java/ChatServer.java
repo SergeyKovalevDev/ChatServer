52a4ce41a299fb9e9811c678beef971de22a23ef
Sergey Kovalev <dip.deep@mail.ru>
2022-02-08 14:39:42 +0300

    modified classes ChatServer, Client

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,24 +1,44 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer {
-    public static void main(String[] args) throws IOException {
+    List<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+
+    public ChatServer() throws IOException {// ненадежный конструктор???
         // создаем серверный сокет на порту 1234
-        ServerSocket server = new ServerSocket(1234);
+        serverSocket = new ServerSocket(1234);
+    }
+
+    void sendAll(String message) {
+        for (Client client :
+                clients) {
+            client.receive(message);
+        }
+    }
+
+    public void run() {
         while(true) {
             System.out.println("Waiting...");
-            // ждем клиента из сети
-            Socket socket = server.accept();
-            System.out.println("Client connected!");
-            // создаем клиента на своей стороне
-            Client client = new Client(socket);
-            // запускаем поток
-            Thread thread = new Thread(client);
-            thread.start();
+            try {
+                // ждем клиента из сети
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                // создаем клиента на своей стороне
+                clients.add(new Client(socket, this));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             //new Thread (new Client(socket)).start();
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
     }
 
 }
