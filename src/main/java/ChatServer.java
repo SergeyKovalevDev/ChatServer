9e6bb7fbcce02645d4d282fedd2df66a61e6dbd8
Sergey Kovalev <dip.deep@mail.ru>
2022-02-08 15:15:31 +0300

    adeed exception handlig in ChatServer constructor

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -5,40 +5,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatServer {
-    List<Client> clients = new ArrayList<>();
-    ServerSocket serverSocket;
+    private final List<Client> clients = new ArrayList<>();
+    private final ServerSocket serverSocket;
 
-    public ChatServer() throws IOException {// ненадежный конструктор???
-        // создаем серверный сокет на порту 1234
-        serverSocket = new ServerSocket(1234);
+    public ChatServer() throws ServerException {
+        try {
+            // creating a server socket on port 1234
+            serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            throw new ServerException("I/O error occurs when opening the socket");
+        } catch (SecurityException e) {
+            throw new ServerException("Security manager exists and its checkListen " +
+                    "method doesn't allow the operation");
+        } catch (IllegalArgumentException e) {
+            throw new ServerException("The port parameter is outside the specified range " +
+                    "of valid port values, which is between 0 and 65535, inclusive");
+        }
     }
 
-    void sendAll(String message) {
+    public void sendAll(String message) {
         for (Client client :
                 clients) {
             client.receive(message);
         }
     }
 
-    public void run() {
+    public void serverStarter() {
         while(true) {
             System.out.println("Waiting...");
             try {
-                // ждем клиента из сети
+                // waiting for a client from the network
                 Socket socket = serverSocket.accept();
                 System.out.println("Client connected!");
-                // создаем клиента на своей стороне
+                // creating a client
                 clients.add(new Client(socket, this));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            //new Thread (new Client(socket)).start();
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        new ChatServer().run();
+    public static void main(String[] args) {
+        try {
+            new ChatServer().serverStarter();
+        } catch (ServerException e) {
+            System.out.println(e.getMessage());
+            System.out.println("The program is stopped");
+        }
     }
-
 }
